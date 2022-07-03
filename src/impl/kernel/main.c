85db03cf1d106d6e3789db1c184b541b2d4286d8
ArdenyUser <71571434+ArdenyUser@users.noreply.github.com>
2022-07-03 17:26:56 -0500

    Update main.c

--- src/impl/kernel/main.c
+++ src/impl/kernel/main.c
@@ -2,7 +2,7 @@
 
 void kernel_main() {
     print_clear();
-    print_set_color(PRINT_COLOR_YELLOW, PRINT_COLOR_BLACK);
-    print_str("apple bottom jeans and boots with th fur");
+    print_set_color(PRINT_COLOR_WHITE, PRINT_COLOR_BLACK);
+    print_str("yispol CORE ; based off David Callanan's OS SERIES");
     print_newline();
 }
